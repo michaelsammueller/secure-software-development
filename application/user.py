38d4b87bca996410e0377ea03048c7116dc96c62
Rachel Doherty <76235546+R-D-Vie@users.noreply.github.com>
2023-06-21 20:40:28 +0200

    Update user.py

--- application/user.py
+++ application/user.py
@@ -1,25 +1,50 @@
+'''
+    This file contains the User class.
+'''
 class User:
-    '''creates user objects dynamically'''
+    '''
+        A class to create user objects.
+    '''
     def __init__(self, first_name, last_name, username, password, secret_phrase):
-        #all attributes come from user/UI
+        '''
+            attributes come from user
+        '''
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
         self.password = password
         self.secret_phrase = secret_phrase
-        #except for last login which comes from logger, not user
+        '''
+            attribute retrieved from logger
+        '''
         self.last_login = str
-
-    def new_user():
-        #can also be defined outside of class
-        #should take user input
-        pass
     
     def login(self):
+        '''
+            A method to handle system logins by users.
+        '''
         pass
 
-    def change_password(self):
+    def change_password(self, new_password):
+        '''
+            A method to handle user password changes.
+        '''
         pass
 
-    def change_phrase(self):
+    def change_phrase(self, new_phrase):
+        '''
+            A method to handle user secret phrase changes.
+        '''
+        pass
+
+    def is_phrase_required(time):
+        '''
+            A method for...
+        '''
+        pass
+
+    def decrypt(secret_phrase):
+        '''
+            A method for...
+        '''
         pass
