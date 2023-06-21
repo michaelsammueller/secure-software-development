5df6cd30bdd4119d9dd29058ad341e68231e530d
Rachel Doherty <76235546+R-D-Vie@users.noreply.github.com>
2023-06-21 20:25:55 +0200

    Create permission.py

---
+++ application/permission.py
@@ -0,0 +1,11 @@
+'''
+    This file contains the Permission class.
+'''
+
+class Permission:
+    
+    def __init__(self, name, description, permission_id):
+        self.name = name
+        self.description = description
+        self.permission_id = permission_id
+        
