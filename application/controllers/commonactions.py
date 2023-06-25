0e1e0b96540887371476f77a7eabfd9399ae88f6
unknown <brad7777@live.com>
2023-06-25 14:01:37 +0100

    stuff missing in the previous commit

--- application/controllers/commonactions.py
+++ application/controllers/commonactions.py
@@ -27,8 +27,7 @@ class CommonActions(object):
         # assert shape of parameter
         try:
             all(new_user_details['user_name'], 
-                new_user_details['user_role'],
-                )
+                new_user_details['user_role'])
         except KeyError:
             self._logger.log({
                 'user' : user,
@@ -59,7 +58,7 @@ class CommonActions(object):
         # add user to database
         return self._db.insert('users', new_user_details)
 
-    def delete_user(self, old_user_name):
+    def delete_user(self, old_user_details):
         '''
             A method for deleting a user from the system.
         '''
@@ -75,17 +74,63 @@ class CommonActions(object):
             'action' : {
                 'type' : action,
                 'parameters' : {
-                    'user_name' : old_user_name
+                    key : value for key, value in old_user_details.items()
                 }
             }
         })
-        pass
+        # delete user from database
+        return self._db.delete('users', old_user_details)
 
-    def add_health_update(self, health_details):
+    def add_health_record(self, new_health_record_details):
         '''
-            A method for adding health details to the system about a user.
+            A method for adding health record details to the system about a user.
+
+            new_user_details interface:
+            {
+                'user_name': user_name,
+                'height'?: height,
+                'weight'?: weight,
+                'blood_type'?: blood_type,
+                'blood_pressure'?: blood_pressure,
+                'heart_rate'?: heart_rate,
+                'body_temperature'?: body_temperature,
+                'diary_entry'?: diary_entry
+            }
         '''
-        pass
+        action = 'add_new_health_record'
+        user = self._login_service.get_logged_in_user()
+        # assert shape of parameter
+        try:
+            all(new_health_record_details['user_name'])
+        except KeyError:
+            self._logger.log({
+                'user' : user,
+                'activity_type' : 'event',
+                'severity' : 'warning',
+                'event' : {
+                    'type' : 'add_new_health_record_key_error',
+                    'parameters' : {
+                        key : value for key, value in new_health_record_details.items()
+                    }
+                }
+            })
+            return False
+        # assert permission for action
+        if not self._authorisation_service.check_permission(action, user):
+            return False
+        # log action
+        self._logger.log({
+            'user' : user,
+            'activity_type' : 'action',
+            'action' : {
+                'type' : action,
+                'parameters' : {
+                    key : value for key, value in new_health_record_details.items()
+                }
+            }
+        })
+        # add user to database
+        return self._db.insert('users', new_health_record_details)
 
     def view_health_records(self, request_details):
         '''
