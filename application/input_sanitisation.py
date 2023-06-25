04735371d14c4322217f423e064498c403316360
Michael Sammueller <34138597+michaelsammueller@users.noreply.github.com>
2023-06-25 15:57:44 +0300

    Update input_sanitisation.py

--- application/input_sanitisation.py
+++ application/input_sanitisation.py
@@ -1,6 +1,7 @@
 # Imports
 from imports import re
 
+
 # Input Sanitisation Service Class
 class Input_Sanitisation_Service:
 
@@ -10,17 +11,16 @@ class Input_Sanitisation_Service:
         sanitized_input = user_input
 
         for keyword in sql_keywords:
-            sanitized_input = sanitized_input.replace(keyword, '') # Replace SQL keywords with empty string
+            # Replace SQL keywords with empty string
+            sanitized_input = sanitized_input.replace(keyword, '')
 
         return sanitized_input
-
 
     def filter_special_characters(self, user_input):
         """Filters special characters from user input"""
-        sanitized_input = re.sub(r'[^\w\s]', '', user_input) # Replace special characters with empty string
+        sanitized_input = re.sub(r'[^\w\s]', '', user_input)
         return sanitized_input
 
-    
     def assert_input_size(self, user_input, min_size, max_size):
         """Asserts that the length of input is within specified range"""
         if len(user_input) < min_size or len(user_input) > max_size:
@@ -31,11 +31,11 @@ class Input_Sanitisation_Service:
     def assert_number(self, user_input):
         """Asserts that input is a number"""
         try:
-            float(user_input) # Convert input to float
+            float(user_input)  # Convert input to float
         except ValueError:
             raise ValueError("Input must be a number.")
 
     def match_pattern(self, user_input, pattern):
         """Matches input against specified pattern"""
-        match = re.match(pattern, user_input) 
-        return bool(match) # Returns True if match is found, False otherwise
+        match = re.match(pattern, user_input)
+        return bool(match)  # Returns True if match is found, False otherwise
