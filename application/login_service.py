88ba0146cfb03f6e47d5f1a015ede3f32d343c51
Michael Sammueller <34138597+michaelsammueller@users.noreply.github.com>
2023-06-25 14:44:52 +0300

    Update login_service.py

--- application/login_service.py
+++ application/login_service.py
@@ -1,3 +1,6 @@
+# Imports
+from imports import sqlite3, bcrypt
+
 # Login Class
 class Login_Service:
     def __init__(self, username, password):
@@ -6,5 +9,22 @@ class Login_Service:
     
     def authenticate_user_credentials(self, username, password):
         """Authenticate user credentials"""
-        pass
+        connect = sqlite3.connect('data/securespace.db')
+        cursor = connect.cursor()
 
+        # Check if the username exists in the database and retrieve the stored password and salt
+        cursor.execute('SELECT password, salt FROM users WHERE username = ?', (username,))
+        result = cursor.fetchone()
+        if result:
+            stored_password = result[0]
+            salt = result[1]
+            # Hash the entered password with the stored salt
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt.encode('utf-8'))
+            # Compare the hashed password with the stored password
+            if stored_password == hashed_password:
+                print('Login successful!\n')
+                return True
+        print("Invalid username or password.\n")
+        connect.close()
+        return False
+        
