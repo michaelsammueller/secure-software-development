5d49eb7bcddce31a065218f6ce345ff0412b13f4
Michael Sammueller <34138597+michaelsammueller@users.noreply.github.com>
2023-06-29 19:34:18 +0300

    Update main.py

--- application/main.py
+++ application/main.py
@@ -1,1 +1,24 @@
+"""
+    Contains the main function for the program.
+"""
 
+# Imports
+from commandline_interface import CommandLineInterface
+from user import User
+
+# Insert test user
+#john_doe = User('john doe', '1', '30061998', '1', '1', 'john.doe', b'$2b$12$1sloC3lxVFlrwguDUNmT8O.QAKu6uSxUtyd1EkvsduH7ov9Oyqm.O')
+
+#john_doe.add_user()
+
+
+# Main Function
+def main():
+    """Main function"""
+    # Create instance of Commandline_Interface class
+    commandline_interface = CommandLineInterface()
+    commandline_interface.display_main_menu()
+
+# Run Program
+if __name__ == "__main__":
+    main()
