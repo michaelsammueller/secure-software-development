37c42202d0ef5ff05925f4f09e08a9c53aff915a
unknown <brad7777@live.com>
2023-06-29 22:28:31 +0100

    set up classes for testing sensor functionality

---
+++ application/tests/integration/mocks.py
@@ -0,0 +1,40 @@
+class MockAuthorisationService:
+    '''
+        A class for mocking the authorisation service.
+    '''
+    def check_permission(self, *args, **kwargs):
+        '''
+        A method for mocking authorisation.
+        '''
+        return True
+    
+class MockLogger:
+    '''
+        A class for mocking the logger.
+    '''
+    def log(self, *args, **kwargs):
+        '''
+        A method for mocking logging.
+        '''
+        return True
+
+class MockLoginService:
+    '''
+        A class for mocking the login service.
+    '''
+    def login(self, *args, **kwargs):
+        '''
+        A method for mocking a login.
+        '''
+        return True
+    
+class MockUser:
+    '''
+        A class for mocking a user.
+    '''
+
+    def get_name(self, *args, **kwargs):
+        '''
+        A method for mocking a login.
+        '''
+        return "Brad"
