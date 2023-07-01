cc07ecbfce8771132dc5893b0cab03fdc85d58a4
Michael Sammueller <34138597+michaelsammueller@users.noreply.github.com>
2023-07-01 22:39:26 +0300

    Update commandline_interface.py
    --------
    ADDED
    --------
    
    - connect_sanitisation_service() - Connects an instance of the Sanitisation_Service class to the CLI
    
    - connect_encryption_service() - Connects an instance of the Encryption_Service class to the CLI
    
    - display_test_menu() - Displays a test menu used for testing other methods; will be removed later
    
    ----------
    UPDATES
    ----------
    
    Updated all other methods to work with the new "connection" interface. Removed imports from other ".py" files.

--- application/commandline_interface.py
+++ application/commandline_interface.py
@@ -3,24 +3,25 @@
 """
 
 # Imports
-from imports import getpass, Login_Service, User, Input_Sanitisation_Service
+import getpass
 
 # CommandLineInterface class
 # This class will be responsible for handling user input
 class CommandLineInterface:
-    #def __init__(self, selection, confirmation, user):
+    #def __init__(self):
+        #self.login_service = None # Initialise login_service attribute
         #self.selection = selection
         #self.confirmation = confirmation
         #self.user = User()  # Will be an instance of the User class
 
-    def greeting(self):
+    def greeting(self, username): # TESTED AND WORKING
         """Display greeting message"""
         print(f"""
         -------------------------
-        Welcome, {self.user.username}!
+        Welcome, {username}!
         -------------------------\n""")
 
-    def get_user_information(self, attribute=None):
+    def get_user_information(self, attribute=None): # TODO: TEST AND DEBUG
         """
             Retrieve user information.
             Args:
@@ -34,21 +35,23 @@ class CommandLineInterface:
         else:
             return getattr(self.user, attribute)
 
-    def request_login_details(self):
+    def request_login_details(self): # TESTED AND WORKING
         """Request user login details"""
         username = input("Username: ")
         password = getpass.getpass("Password: ", stream=None)
         return username, password
 
-    def ask_for_selection(self):
+    def ask_for_selection(self): # TESTED AND WORKING
         """Request user selection"""
         selection = input("Enter your selection: ")
+        print("\n")
         return selection
 
-    def ask_for_confirmation(self):
+    def ask_for_confirmation(self): # TESTED AND WORKING
         """Request user confirmation"""
         while True:
-            confirmation = input("Enter Y to confirm or N to cancel: ")
+            print("Enter Y to confirm or N to cancel.")
+            confirmation = self.ask_for_selection()
             if confirmation.upper() == 'Y':
                 return True
             elif confirmation.upper() == 'N':
@@ -56,17 +59,51 @@ class CommandLineInterface:
             else:
                 print("Invalid selection. Please enter 'Y' or 'N'.\n")
 
-    def display_user_menu(self, username):
+    def display_user_menu(self, username): # TESTED AND WORKING
         """Display user menu options"""
-        self.greeting()
-
+        self.greeting(username) # Display greeting message
         while True:
-            pass  # Create an instance of the "Action_Controller" class
+            options = self.action_controller.get_actions()
+            for i, option in enumerate(options):
+                print(f"{i + 1}. {option}")
+        
+            # Request user selection
+            selection = self.ask_for_selection()
+            # Handle user selection
+            try:
+                selection = int(selection)
+            except:
+                print("Invalid selection.\n")
+                continue
+            if selection <= len(options):
+                # Get additional paramaters from user 
+                params = self.action_controller.get_action_params(options[selection - 1])
+                details = {}
+                print("\nRequesting details...")
+                for param in params:
+                    if not param[1]: # only field name provided
+                        print(f"{param[0]}")
+                        details[param[0]] = self.ask_for_selection()
+                    else: # field name and options provided
+                        print(f"\nOptions for {param[0]}: {param[1]}")
+                        details[param[0]] = self.ask_for_selection()
+                # Perform action
+                results = self.action_controller(options[selection - 1], details)
+                print("\nResults...")
+                print(f"{[f'{key}: {value}' for key, value in results.items()]}")
+                # Ask to continue
+                print("\nWould you like to continue?")
+                if self.ask_for_confirmation():
+                    continue
+                else:
+                    break
+            else:
+                print("Invalid selection.\n")
 
-    def display_main_menu(self):
+    def display_main_menu(self): # TESTED AND WORKING
         """Display main menu options"""
         while True:
-            print("1. Login")
+            print("\n1. Login")
             print("2. Exit\n")
 
             # Request user selection
@@ -75,27 +112,27 @@ class CommandLineInterface:
             # Handle user selection
             if selection == '1':
                 username, password = self.request_login_details()  # Request user login details
-                # Create instance of Login_Service class
-                login_service = Login_Service(username, password)
-                if login_service.login():
-                    self.display_user_menu()
+                # Handle Login
+                if self.login_service.login(username, password):
+                    #self.display_user_menu(username)
+                    self.display_test_menu(username)
                 else:
-                    print("Unable to login.\n")
+                    # Create logger to log failed login attempt
+                    pass
             elif selection == '2':
+                # Handle Exit
                 print("Exiting...\n")
-                break
+                return True # Confirms exit
             else:
                 print("Invalid selection.\n")
 
     # Will live in here for now
-    def change_password(self):
+    def change_password(self): # TESTED AND WORKING
         """Changes user password"""
         # Ask user for reauthentication
         username, password = self.request_login_details()
-        # Create instance of Login_Service class
-        login_service = Login_Service(username, password)
         # Authenticate user credentials
-        if login_service.authenticate_user_credentials():
+        if self.login_service.login(username, password):
             # Ask user for new password
             new_password = getpass.getpass("New Password: ", stream=None)
             # Ask user to confirm new password
@@ -103,7 +140,7 @@ class CommandLineInterface:
             # Check if new password matches confirmation
             if new_password == new_password_confirmation:
                 # Sanitise password
-                new_password = Input_Sanitisation_Service.filter_special_characters(new_password)
+                new_password = self.sanitisation_service.filter_special_characters(new_password)
                 # Check if new password is the same as the old password
                 if new_password == password:
                     print("New password cannot be the same as the old password.\n")
@@ -111,10 +148,9 @@ class CommandLineInterface:
                     # Ask user for phrase
                     phrase = input("Enter your phrase: ")
                     # Authenticate phrase
-                    if login_service.authenticate_phrase(phrase):
+                    if self.login_service.authenticate_phrase(phrase):
                         # Change password
-                        login_service.change_password(new_password)
-                        print("Password changed successfully.\n")
+                        self.login_service.change_password(new_password)
                         # Create logger to log password change
                         return True
                     else:
@@ -127,22 +163,22 @@ class CommandLineInterface:
                 return False
 
     # Will live in here for now
-    def change_phrase(self):
+    def change_phrase(self): # TESTED AND WORKING
         """Changes user phrase"""
         # Ask user for reauthentication
         username, password = self.request_login_details()
-        # Create instance of Login_Service class
-        login_service = Login_Service(username, password)
         # Authenticate user credentials
-        if login_service.authenticate_user_credentials():
+        if self.login_service.login(username, password):
             # Ask user for new phrase
             new_phrase = input("New Phrase: ")
             # Ask user to confirm new phrase
             new_phrase_confirmation = input("Confirm New Phrase: ")
             # Check if new phrase matches confirmation
             if new_phrase == new_phrase_confirmation:
-                    new_phrase = Input_Sanitisation_Service.sanitise_phrase(new_phrase)
-                    login_service.change_phrase(new_phrase)
+                    print(f"New phrase: {new_phrase}\n") # TODO: Remove this line
+                    sanitised_phrase = self.sanitisation_service.sanitise_phrase(new_phrase)
+                    print(f"Sanitised phrase: {sanitised_phrase}\n") # TODO: Remove this line
+                    self.login_service.change_phrase(sanitised_phrase)
                     print("Phrase changed successfully.\n")
                     # Create logger to log phrase change
                     return True
@@ -154,3 +190,50 @@ class CommandLineInterface:
             print("Incorrect credentials.\n")
             # Create logger to log failed login attempt
             return False
+        
+    def connect_login_service(self, login_service): # TESTED AND WORKING
+        """Connects the login service"""
+        self.login_service = login_service
+
+    def connect_action_controller(self, action_controller): # TESTED AND WORKING
+        """Connects the action controller"""
+        self.action_controller = action_controller
+    
+    def connect_sanitisation_service(self, sanitisation_service): # TESTED AND WORKING
+        """Connects the sanitisation service"""
+        self.sanitisation_service = sanitisation_service
+    
+    def connect_encryption_service(self, encryption_service):
+        """Connects the encryption service"""
+        self.encryption_service = encryption_service
+    
+    def display_test_menu(self, username): # FOR TESTING ONLY: TODO REMOVE THIS
+        print("Test Menu")
+        self.greeting(username)
+        while True:
+            print("\n1. Change Password")
+            print("\n2. Change Phrase")
+            print("\n3. Ask for confirmation")
+            print("\n4. Retrieve user attributes")
+            print("\n99. Exit\n")
+
+            # Request user selection
+            selection = self.ask_for_selection()
+
+            # Handle user selection
+            if selection == '1':
+                self.change_password()
+            elif selection == '2':
+                self.change_phrase()
+            elif selection == '3':
+                self.ask_for_confirmation()
+            elif selection == '4':
+                attribute = input("Attribute: ")
+                if attribute is not None:
+                    print(self.get_user_information(attribute))
+                elif attribute == "":
+                    print(self.get_user_information())
+            elif selection == '99':
+                # Handle Exit
+                print("Exiting...\n")
+                break
