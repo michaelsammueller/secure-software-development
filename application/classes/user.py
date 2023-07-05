6bd83d2392ad367487885179877f68654a3bf2c2
Rachel Doherty <76235546+R-D-Vie@users.noreply.github.com>
2023-07-05 20:50:26 +0200

    Update user.py
    updates post meeting 05.07.23.

--- application/classes/user.py
+++ application/classes/user.py
@@ -1,6 +1,7 @@
 '''
     This file contains the User class.
 '''
+# leave imports in for now
 import uuid
 import os
 import sys
@@ -13,52 +14,50 @@ class User:
     '''
         A class to create user objects.
     '''
-    def __init__(self, name, code, dob, role_id, country_id, username, password):
-        self.__name = name
-        self.__code = code
-        self.__dob = dob
-        self.__role_id = role_id
-        self.__country_id = country_id
-        self.__username = username
-        self.__password = password
+    def __init__(self):
+        self.__name = self.__name
+        self.__code = self.__code
+        self.__dob = self.__dob
+        self.__role_id = self.__role_id
+        self.__country_id = self.__country_id
+        self.__username = self.__username
+        self.__password = self.__password
         self.__uuid = uuid.uuid4()
-        self.__created_at = None
-        self.__updated_at = None
-        self.__last_login_at = None
+        
 
         # initialise instance of DBManager
-        self.db_manager = DBManager()
+        self.__db_manager = DBManager()
 
         # set current_time to now
-        self.current_time = datetime.datetime.now()
+        self.__current_time = datetime.datetime.now()
 
-    def add_user(self):
-        # update created_at and updated_at attributes
-        if self._created_at is None:
-            self._created_at = self.current_time
-        self._updated_at = self.current_time
-
+    def add_user(self, name, code, dob, role_id, country_id, username, password):
         # perform database query to save user attributes.
         query = "INSERT INTO users (uuid, name, code, dob, role_id, \
-            country_id, username, password) VALUES ?, ?, ?, ?, ?, ?, ?, ?)"
-        values = (str(self._uuid), self._name, self._code, self._dob, self._role_id,
-                  self._country_id, self._username, self._password, self._created_at)
+            country_id, username, password, updated_at) VALUES ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        values = (str(uuid.uuid4()), name, code, dob, role_id,
+                  country_id, username, password, self.__current_time)
 
         # call do_insert method from DBmanager.
-        self.db_manager.do_insert(query, [values], dry=False)
+        self.__db_manager.do_insert(query, [values], dry=False)
 
     def update_user(self):
         # update the 'updated_at' attribute.
-        self._updated_at = self.current_time
+        self.__updated_at = self.__current_time
 
         # perform database query to update user attributes.
         query = "UPDATE users SET name=?, code=?, dob=?, role_id=?, \
             country_id=?, username=?, password=?, updated_at=? WHERE uuid=?"
-        values = (self._name, self._code, self._dob, self._role_id, self._country_id,
-                  self._username, self._password, self._updated_at, str(self._uuid))
+        values = (self.__name, self.__code, self.__dob, self.__role_id, self.__country_id,
+                  self.__username, self.__password, self.__updated_at, str(self.__uuid))
 
         # call do_update method from DBmanager.
-        self.db_manager.do_update(query, values)
+        self.__db_manager.do_update(query, values)
 
     def delete_user(self):
-        pass
+        # perform database query to identify row to delete
+        query = "DELETE FROM users WHERE uuid=?"
+        values = (str(self.__uuid))
+        
+        # call do_delete method from DBmanager
+        self.__db_manager.do_delete(query, values)
