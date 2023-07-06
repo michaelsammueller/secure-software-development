709eb85f4faab64fc778dcd598d791881f51651c
unknown <brad7777@live.com>
2023-07-06 21:25:03 +0100

    gave user class a view all users method for testing

--- application/classes/models/user.py
+++ application/classes/models/user.py
@@ -9,15 +9,28 @@ class User:
     '''
 
     def add_user(self, user_details):
+        '''
+            Adds a user to the database
+        '''
         # perform database query to save user attributes.
         query = "INSERT INTO users (uuid, name, code, dob, role_id, \
             country_id, username, password) VALUES ?, ?, ?, ?, ?, ?, ?, ?)"
-        values = (str(uuid.uuid4()), user_details['name'], user_details['code'], user_details['date of birth'], user_details['role'],
+        values = (str(uuid.uuid4()), user_details['name'], user_details['username'], user_details['date of birth'], user_details['role'],
                   user_details['country of employment'], user_details['username'], user_details['password'])
 
         # call do_insert method from DBmanager.
         self.__db_manager.do_insert(query, [values], dry=False)
 
+    def view_all_users(self):
+        '''
+            View users from the database
+        '''
+        # perform database query to view user attributes.
+        query = "SELECT * FROM users"
+        where = ()
+
+        # call do_select method from DBManager.
+        return self.__db_manager.do_select(query, where)
 
     def connect_db_manager(self, db_manager):
         '''
