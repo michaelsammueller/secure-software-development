30c4cf32aa3ff197db73d020c721116a9dfae711
unknown <brad7777@live.com>
2023-07-08 14:40:19 +0100

    add missing paranthesis to user class

--- application/classes/models/user.py
+++ application/classes/models/user.py
@@ -14,12 +14,12 @@ class User:
         '''
         # perform database query to save user attributes.
         query = "INSERT INTO users (uuid, name, code, dob, role_id, \
-            country_id, username, password) VALUES ?, ?, ?, ?, ?, ?, ?, ?)"
+            country_id, username, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?);"
         values = (str(uuid.uuid4()), user_details['name'], user_details['username'], user_details['date of birth'], user_details['role'],
                   user_details['country of employment'], user_details['username'], user_details['password'])
 
         # call do_insert method from DBmanager.
-        self.__db_manager.do_insert(query, [values], dry=False)
+        self.__db_manager.do_insert(query, values, dry=False)
 
     def view_all_users(self):
         '''
