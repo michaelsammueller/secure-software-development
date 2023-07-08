a902ef44fee3d3f0264334c1bcca060f8dca6b0f
unknown <brad7777@live.com>
2023-07-08 22:36:04 +0100

    main.py is runnable

--- application/main.py
+++ application/main.py
@@ -2,13 +2,14 @@
     Contains the main function for the program.
 """
 # Imports
-from imports import CommandLineInterface, User, ActionsController, Input_Sanitisation_Service, Encryption_Service, Login_Service
+from imports import CommandLineInterface, User, ActionsController, Input_Sanitisation_Service, Encryption_Service, Login_Service, Logger
+from tests.integration.mock import MockAuditor
 
 # Insert test user
 
-john_doe = User('john doe', '1', '30061998', '1', '1', 'john.doe', b'$2b$12$1sloC3lxVFlrwguDUNmT8O.QAKu6uSxUtyd1EkvsduH7ov9Oyqm.O', 'tryme')
+# john_doe = User('john doe', '1', '30061998', '1', '1', 'john.doe', b'$2b$12$1sloC3lxVFlrwguDUNmT8O.QAKu6uSxUtyd1EkvsduH7ov9Oyqm.O', 'tryme')
 
-john_doe.add_user()
+# john_doe.add_user()
 
 
 # Main Function
@@ -19,13 +20,15 @@ def main():
     action_controller = ActionsController()
     sanitisation_service = Input_Sanitisation_Service()
     encryption_service = Encryption_Service()
-    auditor = Auditor(warning_file='warnings.txt', danger_handler='danger.txt')
-    logger = Logger(log_file='logs.txt', auditor=auditor, encryption_service=encryption_service)
+    auditor = MockAuditor
+    logger = Logger(log_file='logs.txt')
     commandline_interface.connect_action_controller(action_controller)
     commandline_interface.connect_login_service(login_service)
     commandline_interface.connect_sanitisation_service(sanitisation_service)
     commandline_interface.connect_encryption_service(encryption_service)
     commandline_interface.connect_logger(logger)
+    logger.connect_auditor(auditor)
+    logger.connect_encryption_service(encryption_service)
     login_service.connect_encryption_service(encryption_service)
     commandline_interface.display_main_menu()
 
